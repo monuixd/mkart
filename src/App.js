002c3f9c3a4194a7439a2e232082f48d34f71615
MONUiXD <42413791+monuixd@users.noreply.github.com>
2022-03-16 16:44:18 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Header from './components/header';
 import Home from './components/home';
 import Footer from './components/footer';
@@ -7,50 +7,52 @@ import AboutUs from './components/aboutus';
 import ProductList from './components/product-list';
 import ProductDetail from './components/product-detail';
 import ContactUs from './components/contactus';
+import Products from "./components/Hooks/Products";
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        products: [],
-        loader: ""
+      products: [],
+      loader: ""
 
     }
 
     this.handleCallback = this.handleCallback.bind(this)
   }
 
-handleCallback (childData) {
-  console.log("APP CONS", {childData})
-  this.setState ({
-    products:childData.productCatArray
-  })
-}
+  handleCallback(childData) {
+    //console.log("APP CONS", { childData })
+    this.setState({
+      products: childData.productCatArray
+    })
+  }
 
- render(){
+  render() {
 
-  console.log("APP Render", this.state.products)
-  return (
-    <div className="App">
+    //console.log("APP Render", this.state.products)
+    return (
+      <div className="App">
 
-      <Header />
-      <Switch>
-        <Route exact path='/'
-          render={(props) => <Home {...props} parentCallback = {this.handleCallback} />}
-        />
-        <Route path='/about-us' component={AboutUs} />
-        <Route path="/product-list" component={ProductList} />
-        <Route path="/product-detail" component={ProductDetail} />
-        <Route path="/contact-us" component={ContactUs} />
+        <Header />
+        <Switch>
+          <Route exact path='/'
+            render={(props) => <Home {...props} parentCallback={this.handleCallback} />}
+          />
+          <Route path='/about-us' component={AboutUs} />
+          <Route path="/product-list" component={ProductList} />
+          <Route path="/product-detail" component={ProductDetail} />
+          <Route path="/contact-us" component={ContactUs} />
+          <Route path="/hooks-products" component={Products} />
 
-      </Switch>
-      <Footer prodCat={this.state.products}  />
+        </Switch>
+        <Footer prodCat={this.state.products} />
 
-      {/* <Counter /> */}
-    </div>
-  );
-}
+        {/* <Counter /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
